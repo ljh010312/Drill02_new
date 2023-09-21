482a4e684c8a8935165ba5c35029c330e82c3956
Lee9747 <leejunho32@naver.com>
2023-09-21 12:03:01 +0900

    캔버스 중심 원 운동 완료

--- character_grass.py
+++ character_grass.py
@@ -1,4 +1,5 @@
 from pico2d import *
+import math
 
 open_canvas()
 
@@ -12,6 +13,16 @@ delay(1)
 
 def run_circle():
     print('CIRCLE')
+    r, cx, cy = 200, 400, 300
+    for deg in range(0, 360, 5):
+        x = cx + r * math.cos(math.radians(deg))
+        y = cy + r * math.sin(math.radians(deg))
+        clear_canvas_now()
+        grass.draw_now(400, 30) 
+        character.draw_now(x, y)
+        delay(0.1)
+
+    
     pass
 
 def run_rectangle():
@@ -21,6 +32,7 @@ def run_rectangle():
 while True:
     run_circle()
     run_rectangle()
+    break
 
 
 close_canvas()
